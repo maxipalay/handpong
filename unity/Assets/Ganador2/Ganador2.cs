7ab9b7da768855d7d4bbcd44da111ea0a3c73e33
maxipalay <maximilianopalay@gmail.com>
2020-05-10 18:58:45 +0200

    final

---
+++ unity/Assets/Ganador2/Ganador2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Ganador2 : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Volver()
+    {
+       // Debug.Log("SALOR");
+  SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex -3);
+    }
+}
---
+++ unity/Assets/Pelota_Controlador.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pelota_Controlador : MonoBehaviour {
+    private Rigidbody2D rb;
+    int velocidad = 1;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(5f, 5f);
+
+        StartCoroutine(Pause());
+
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.x >= 17f)
+        {
+            this.transform.position = new Vector3(0f, 0f, 0f);
+            velocidad += 1;
+            StartCoroutine(Pause());
+        }
+        if (this.transform.position.x <= -17f)
+        {
+            this.transform.position = new Vector3(0f, 0f, 0f);
+            StartCoroutine(Pause());
+        }
+
+
+    }
+
+
+
+
+
+
+    IEnumerator Pause()
+    {
+
+
+        rb.velocity = new Vector2(0f, 0f);
+        yield return new WaitForSeconds(2);
+        rb.velocity = new Vector2(6f*velocidad, 6f*velocidad);
+
+    }
+}
